69d215a83b9f094d61e0011fc37761fd77a721a6
jeremnds <jrmymnds@gmail.com>
2024-08-27 12:41:52 +0200

    feat: add inputs form

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,5 +1,17 @@
+"use client";
+
 import Container from "@/src/components/atoms/Container";
+import InputForm from "@/src/components/organisms/InputForm";
 
 export default function Page() {
-  return <Container className="mt-20">generate</Container>;
+  return (
+    <Container className="mt-16">
+      <div className="flex flex-col md:flex-row">
+        <p className="order-2 md:order-1 md:basis-1/3">input generation</p>
+        <div className="order-1 md:order-2 md:basis-2/3">
+          <InputForm />
+        </div>
+      </div>
+    </Container>
+  );
 }
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -3,7 +3,7 @@ import Container from "../atoms/Container";
 
 export default function Header() {
   return (
-    <div className="flex h-16 items-center bg-purple-400">
+    <div className="flex min-h-12 items-center bg-purple-400">
       <Container>
         <Link href="/" className="text-xl text-white">
           Form<span className="font-bold text-purple-800">Gen</span>
